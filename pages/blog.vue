f21ceb3a87a524b777a9ce28a7d50d71dcb7e7e6
How Bizarre <howbizarre@gmail.com>
2024-05-27 09:42:41 +0300

    chore: Remove unused ContentNavigation component from blog page

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,16 +1,5 @@
 <template>
   <main>
-    <ContentNavigation v-slot="{ navigation }">
-      <ul>
-        <li v-for="link of navigation" :key="link._path">
-          <template v-for="dir in link.children" :key="dir._path">
-            <NuxtLink :to="dir._path">
-              {{ dir.title }}
-            </NuxtLink>
-          </template>
-        </li>
-      </ul>
-    </ContentNavigation>
     <h1>Blog Content</h1>
   </main>
 </template>
