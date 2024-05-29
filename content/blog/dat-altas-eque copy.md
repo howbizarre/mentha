5f5b947a9e6bda873d2f219d696e5afae11eca8c
How Bizarre <howbizarre@gmail.com>
2024-05-29 11:20:05 +0300

    refactor: Add article date to blog page navigation links

---
+++ content/blog/dat-altas-eque copy.md
@@ -0,0 +1,51 @@
+---
+title: 'Nec moram valens praemia tenuata et gerunt'
+description: 'meta description of the page'
+date: '2024-05-29T12:14:00.771Z'
+draft: false
+---
+
+# Nec moram valens praemia tenuata et gerunt
+
+*Lorem* markdownum simul, est aures quoniam omnes latet, auro Pelasgi, et hic.
+Epidauria remisit: ereptam fuerit et spectacula alto et novi de paveant motis
+futuro ausum coniuge feris tentoria tremuisse. Solebam Aeetias Troiane; haec
+modo? Incurvo iam quem, emittere Symplegadas ecce, dementer. Samon dares: Mopso
+viri fronte, dentes quae pericula, auctor Titania oves.
+
+<!-- more -->
+
+1. Sed quos clarum
+2. Liberat vocant et veste
+3. Felicem luce canes adpositas artis harundine
+4. Retia castos
+
+## In viperei litoraque fatalis falsum nobis corpore
+
+Quantus nota est senex ferire auctor Ophiusiaque moenibus, pressa fato
+pectoraque *laedat*! Illa ille attulerat; Delphice quoque deserit. Aret inops
+vincere parenti? Et hanc tanto simul signaverat **manus**, in sentiri et segetes
+negare, trepidante ductum.
+
+## Naribus opes dixit
+
+Sortes revulsit carissime te pacis ara occidat liliaque: dicta iuvat bella!
+Facti leves uberior teste. Humo [copia quam
+manifesta](http://www.torpetisseque.io/dixit.aspx). Denique instituit ille:
+illuc est [Saturnia](http://sirinosque-omnia.net/effugit.html) despicit Hectoris
+minimum huc et revincta cupidine flammifera saltu consiliis Iovis.
+
+## Diti ter aestatem a atque
+
+Sit vitis sistrorum, harenam, tritumque cadunt sistrorum animi quamlibet
+biformis venis: spem baculo ferre moriens inferias. *Assaracus In vacuas*
+deieci, vos vidisse nulla sua tenuissima domos colat deus amor.
+
+Ab per auctore vultu vulneret apertis sedesque gemitu telae visum aglauros nomen
+patriae pudorem: cur [qui](http://hunc-virides.net/). Caputque actis, adiciam;
+nunc iussus petisti Rhoetus!
+
+Mortisque ferocia medium, ut *matri navale* auctor saepe vitare ad vocat iubebat
+nil, ora. Rectum servatae: dum ranae sagitta renovamine occupat cacumine rostro.
+Tantum a quaedam ardent felix auroque tabularia vipereas avia Gortyniaco
+discede.
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -6,11 +6,11 @@ const path = `/blog/${slug.value}`;
 const { data: surround } = await useAsyncData(`[slug-${slug.value}]`, () => {
   return import.meta.dev
     ? queryContent()
-      .only(['title', '_path'])
+      .only(['title', '_path', 'date'])
       .findSurround(path, { before: 1, after: 1 })
     : queryContent()
       .where({ draft: false })
-      .only(['title', '_path'])
+      .only(['title', '_path', 'date'])
       .findSurround(path, { before: 1, after: 1 })
 }, { default: () => [] });
 
@@ -38,23 +38,29 @@ const [prev, next] = surround.value || [];
 
     <div class="grid gap-8 sm:grid-cols-2">
       <ContentRenderer v-if="next" :value="next">
-        <NuxtLink :to="`${next._path}`" class="flex justify-between items-start px-6 py-8 border rounded-lg !border-gray-50 dark:!border-gray-950 hover:bg-gray-50/50 dark:hover:bg-gray-950/50 group transition-colors duration-300">
+        <NuxtLink :to="`${next._path}`" class="flex justify-start items-start px-6 py-8 border rounded-lg !border-gray-50 dark:!border-gray-950 hover:bg-gray-50/50 dark:hover:bg-gray-950/50 group transition-colors duration-300">
           <div class="mr-2 inline-flex items-center rounded-full p-1.5 bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 mb-4">
-            <Icon name="eva:arrowhead-left-fill" size="24" />
+            <Icon name="material-symbols:arrow-left-alt-rounded" size="24" />
           </div>
 
-          <h1 class="mb-0">{{ next.title }}</h1>
+          <div>
+            <ClientOnly><small>{{ articleLocaleDate(next.date) }}</small></ClientOnly>
+            <h1 class="mb-0">{{ next.title }}</h1>
+          </div>
         </NuxtLink>
       </ContentRenderer>
 
       <div v-else class="hidden sm:block px-6 py-8">&nbsp;</div>
 
       <ContentRenderer v-if="prev" :value="prev">
-        <NuxtLink :to="`${prev._path}`" class="flex justify-between items-start px-6 py-8 border rounded-lg !border-gray-50 dark:!border-gray-950 hover:bg-gray-50/50 dark:hover:bg-gray-950/50 group transition-colors duration-300 text-right">
-          <h1 class="mb-0">{{ prev.title }}</h1>
+        <NuxtLink :to="`${prev._path}`" class="flex justify-end items-start px-6 py-8 border rounded-lg !border-gray-50 dark:!border-gray-950 hover:bg-gray-50/50 dark:hover:bg-gray-950/50 group transition-colors duration-300 text-right">
+          <div>
+            <ClientOnly><small>{{ articleLocaleDate(prev.date) }}</small></ClientOnly>
+            <h1 class="mb-0">{{ prev.title }}</h1>
+          </div>
 
           <div class="ml-2 inline-flex items-center rounded-full p-1.5 bg-gray-100 dark:bg-gray-800 ring-1 ring-gray-300 dark:ring-gray-700 mb-4">
-            <Icon name="eva:arrowhead-right-fill" size="24" />
+            <Icon name="material-symbols:arrow-right-alt-rounded" size="24" />
           </div>
         </NuxtLink>
       </ContentRenderer>
